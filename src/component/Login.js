0540aa6d3db268b733299ca5bb4931edd7b195b7
kingboseong <rlaqhtjd8001@naver.com>
2024-04-11 17:47:05 +0900

    reacr-4차

--- src/component/Login.js
+++ src/component/Login.js
@@ -2,12 +2,25 @@ import { useRecoilState } from "recoil"
 import { isLogin } from "./StAtom"
 import { useState, useEffect } from "react"
 import LoginForm from "./LoginForm"
+import Logout from "./Logout"
 
 
 export default function Login() {
 
     const [user, setUser] = useState()
     const [isLoginCheck, setIsLoginCheck] = useRecoilState(isLogin);
+
+    const handleLogin = (u) => {
+        localStorage.setItem('user',u) // 로컬 스토리지에 로그인 정보를 박아줌
+        setUser(u)
+        setIsLoginCheck(true)
+    }
+
+    const handleLogout = () => {
+        localStorage.removeItem('user')
+        setUser(null)
+        setIsLoginCheck(false)
+    }
 
     useEffect(() => {
         const luser = localStorage.getItem('user')
@@ -23,7 +36,8 @@ export default function Login() {
 
     return (
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-            <LoginForm/>
+            {isLoginCheck ? <Logout user={user} handleLogout = {handleLogout}/> : 
+                            <LoginForm handleLogin = {handleLogin}/>}
         </div>
     )
 }
--- src/component/Subway.js
+++ src/component/Subway.js
@@ -1,5 +1,4 @@
 
-
 export default function Subway() {
   return (
     <div>
