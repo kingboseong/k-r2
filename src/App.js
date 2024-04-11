a09fafc8a7559b4b5e264b8be79957c7057cc464
kingboseong <rlaqhtjd8001@naver.com>
2024-04-11 15:10:46 +0900

    react -1차

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route  } from "react-router-dom";
+import Home from "./component/Home";
+import Login from "./component/Login";
+import Subway from "./component/Subway";
+import Nav from "./component/Nav";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/login" element={<Login />}/>
+          <Route path="/subway" element={<Subway />}/>
+        </Routes>
+      </BrowserRouter>
   );
 }
 
